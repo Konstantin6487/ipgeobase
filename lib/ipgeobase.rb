421a75e55059b8df47ebb84b1da0289f9ea10478
Konstantin Bochinin <konstantin6487@rambler.ru>
2021-07-26 23:41:51 +0300

    add entry point

--- lib/ipgeobase.rb
+++ lib/ipgeobase.rb
@@ -2,7 +2,16 @@
 
 require_relative "ipgeobase/version"
 
+# main Module
 module Ipgeobase
+  autoload :Location, "ipgeobase/location"
+  autoload :Parsers, "ipgeobase/parsers"
+  autoload :Address, "ipgeobase/address"
+
   class Error < StandardError; end
-  # Your code goes here...
+
+  def self.lookup(ip)
+    location = Location.new
+    location.lookup ip
+  end
 end
