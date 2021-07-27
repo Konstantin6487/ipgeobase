ae9747aa9778a583134e5ec9ec6b2f88d834c9af
Konstantin Bochinin <konstantin6487@rambler.ru>
2021-07-27 16:21:09 +0300

    refactoring tests

--- test/ipgeobase_test.rb
+++ test/ipgeobase_test.rb
@@ -3,32 +3,28 @@
 require "test_helper"
 
 class IpgeobaseTest < Minitest::Test
-  API_URL = "http://ip-api.com"
+  attr_reader :api_url
 
-  attr_reader :ip, :address
+  def setup
+    @api_url = "http://ip-api.com"
+  end
 
   def test_that_it_has_a_version_number
     refute_nil ::Ipgeobase::VERSION
   end
 
-  def setup
-    @address = read_fixture_file "geobase_raw.xml"
-    @ip = "83.169.216.199"
-
-    url = Addressable::Template.new("#{IpgeobaseTest::API_URL}{/segments*}")
-    url.expand({ segments: ["xml", ip] })
-
-    stub_request(:get, url).to_return(body: address)
-  end
-
   def test_load_address
-    parsed_fixture = Ox.load address, mode: :hash
-    expected = parsed_fixture[:query]
+    file_name = "geobase_raw.xml"
+    address_data_raw = read_fixture_file file_name
+    ext = file_name.split(".").last
+    ip = "83.169.216.199"
+    url = build_url api_url, ip: ip, ext: ext
+    run_stub url, method: :get, body: address_data_raw
+
+    expected = Ox.load(address_data_raw, mode: :hash)[:query]
     actual = Ipgeobase.lookup ip
 
-    assert_equal(
-      [expected[:city], expected[:country], expected[:countryCode], expected[:lat], expected[:lon]],
-      [actual.city, actual.country, actual.countryCode, actual.lat, actual.lon]
-    )
+    %i[city country countryCode lat lon]
+      .each { |field| assert_equal expected[field], actual.instance_variable_get("@#{field}") }
   end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -10,6 +10,15 @@ require "addressable/template"
 
 WebMock.disable_net_connect!
 
+def build_url(api_url, ip:, ext:)
+  template = Addressable::Template.new "#{api_url}{/segments*}"
+  template.expand({ segments: [ext || "xml", ip] })
+end
+
+def run_stub(url, method:, body:)
+  stub_request(method, url).to_return(body: body)
+end
+
 def read_fixture_file(filename)
   File.read(File.dirname(__FILE__) + "/fixtures/#{filename}")
 end
