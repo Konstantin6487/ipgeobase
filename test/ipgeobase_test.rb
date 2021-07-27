e989d7b2a13ea362165c3e78c5907ac096b0bc92
Konstantin Bochinin <konstantin6487@rambler.ru>
2021-07-27 15:24:04 +0300

    add tests

--- test/ipgeobase_test.rb
+++ test/ipgeobase_test.rb
@@ -3,11 +3,30 @@
 require "test_helper"
 
 class IpgeobaseTest < Minitest::Test
+  API_URL = "http://ip-api.com"
+
   def test_that_it_has_a_version_number
     refute_nil ::Ipgeobase::VERSION
   end
 
-  def test_it_does_something_useful
-    assert false
+  def setup
+    @address = read_fixture_file("geobase_raw.xml")
+    @ip = "83.169.216.199"
+
+    url = Addressable::Template.new("#{IpgeobaseTest::API_URL}{/segments*}")
+    url.expand({ segments: ["xml", @ip] })
+
+    stub_request(:any, url).to_return(body: @address)
+  end
+
+  def test_load_address
+    parsed_fixture = Ox.load(@address, mode: :hash)
+    expected = parsed_fixture[:query]
+    actual = Ipgeobase.lookup(@ip)
+
+    assert_equal(
+      [expected[:city], expected[:country], expected[:countryCode], expected[:lat], expected[:lon]],
+      [actual.city, actual.country, actual.countryCode, actual.lat, actual.lon]
+    )
   end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,3 +4,12 @@ $LOAD_PATH.unshift File.expand_path("../lib", __dir__)
 require "ipgeobase"
 
 require "minitest/autorun"
+require "webmock/minitest"
+require "ox"
+require "addressable/template"
+
+WebMock.disable_net_connect!
+
+def read_fixture_file(filename)
+  File.read(File.dirname(__FILE__) + "/fixtures/#{filename}")
+end
